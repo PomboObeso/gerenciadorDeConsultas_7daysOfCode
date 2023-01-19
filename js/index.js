9ab679e2317290983447e3b3c60c0b8aed829887
PomboObeso <matheus.gomes201@gmail.com>
2023-01-19 14:18:00 -0300

    extra 1 - add fundo opaco

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const page = document.getElementsByTagName('body');
+const cardSection = document.querySelectorAll('.card__consulta');
+
+const btnMais = document.querySelector('.rodape__btn--agenda');
+
+console.log(cardSection);
+
+btnMais.addEventListener("click", () => {
+    page[0].classList.add("efeito__opacidade");
+    cardSection[0].classList.add("efeito__opacidade");
+    cardSection[1].classList.add("efeito__opacidade");
+});
+
+
